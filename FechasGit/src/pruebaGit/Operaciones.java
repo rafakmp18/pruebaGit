a0b579f5847d604673b00df3279c11df6ce2ece9
rafa <rafakmp18@gmail.com>
2020-02-07 19:06:01 +0100

    Agregada la clase Operaciones

---
+++ FechasGit/src/pruebaGit/Operaciones.java
@@ -0,0 +1,13 @@
+package pruebaGit;
+
+public class Operaciones {
+
+	int operadorA;
+	int operadorB;
+
+	public void Operaciones(int operadorA, int operadorB) {
+		this.operadorA=operadorA;
+		this.operadorB=operadorB;
+	}
+
+}
